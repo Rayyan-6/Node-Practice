9481d0a5141ab4bba0bb2490dde7bef3061788e6
Muhammad Rayyan <muhd.rayyan.mail@gmail.com>
2025-12-01 18:20:44 +0500

    redis added

---
+++ controllers/redis.js
@@ -0,0 +1,45 @@
+import { createClient } from "redis";
+
+let client;
+
+const initRedisClient = async ()=>{
+    if(!client){
+        client = createClient();
+        client.on("error", ()=> console.log("Error using redis client"))
+    }
+
+    try{
+        await client.connect()
+    }
+    catch(error){
+        console.log("Error occured while initialising redis");
+        throw error;
+    }
+}
+
+
+const getValue = async (key)=>{
+    try {
+        const value = await client.json.get(`user:${key}`)
+        return value;
+
+
+    } catch (error) {
+        console.log("redis get error for ", key)
+        throw error
+    }
+}
+
+
+const setValue = async (key,value)=>{
+    try {
+        const data = await client.json.set(`user: ${key}`,"$", value);
+        return data;
+    } catch (error) {
+        console.log("Error occured while setting value for key: ", key)
+        throw error;
+    }
+}
+
+
+export {initRedisClient, getValue, setValue};
